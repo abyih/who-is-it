ac90d9d67081fcd927d4a98013cea1f751a33d98
Abiy Hailu <abiyhailu120@gmail.com>
2026-06-06 16:34:05 +0300

    fix:make-router-hash

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 // @ts-ignore
 import HomePage from "./routes/home-page.vue";
 // @ts-ignore
@@ -35,6 +35,6 @@ const routes = [
 ];
 
 export const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
